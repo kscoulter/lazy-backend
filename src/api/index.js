49c27d7ad038675d98d47c269d54e4d4a9ef1244
Ksenia Coulter <kseniacoulter@gmail.com>
2017-03-30 09:40:00 -0400

    cleaning up the repo

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,30 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
+import * as googleMaps from '@google/maps';
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
+		console.log(process.env.GOOGLE_PLACE_API_KEY);
+		googleMaps.createClient({
+			key:process.env.GOOGLE_PLACE_API_KEY
+		})
+		googleMaps.places({
+      query: 'fast food',
+      language: 'en',
+      location: [-33.865, 151.038],
+      radius: 5000,
+      minprice: 1,
+      maxprice: 4,
+      opennow: true,
+      type: 'restaurant'
+    })
+    .asPromise()
+    .then(function(response) {
+			console.log(response);
+    })
 		res.json({ version });
 	});
 
